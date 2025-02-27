0d9ebb86dd45d9657fb663e0c56a64a26eac95ae
H4ckVision <engralisubhan@gmail.com>
2025-02-27 22:06:08 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, jsonify
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load your trained ML model
+with open('Crop_Yield_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get input data from the form
+    data = request.json
+    features = [
+        float(data['rainfall']),
+        float(data['soil_quality']),
+        float(data['farm_size']),
+        float(data['sunlight']),
+        float(data['fertilizer'])
+    ]
+
+    # Predict using the model
+    prediction = model.predict([features])
+    output = round(prediction[0], 2)
+
+    return jsonify({'prediction': output})
+
+if __name__ == '__main__':
+    app.run(debug=True)
